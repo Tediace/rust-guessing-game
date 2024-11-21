62e7ea28eacd8d5b09684a69765ca276265f18ea
tediace <acetedi7@gmail.com>
2024-11-21 15:04:12 +0700

    add some useful comments

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-//import module io(input/output)
-//read input from user or write output to console
+// import module io(input/output)
+// read input from user or write output to console
+// additional information (use syntax used for import library is not in prelude in rust)
 use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
-    //let for declaration variable
-    //mut for mutable variable
-    //guess is named variable
-    //string::new() created string blank (kosong)
-    let mut guess = String::new();
+    // let for declaration variable
+    // mut for mutable variable
+    // guess is named variable
+    // string::new() created string blank
+    // Conclusion crate variable to store the user input
+    let mut guess = String::new( );
 
-    //io::stdin() get input from standard input (keyboard)
-    //.read_line(&mut guess) read input until press enter
-    //.expect if error throw message 'Failed to read line'
-    //{guess} variable from input
-    //
+    let apples = 5; // immutable
+    let mut bananas = 5; // mutable
+
+    // io::stdin() get input from standard input (keyboard)
+    // .read_line(&mut guess) read input until press enter
+    // .expect if error throw message 'Failed to read line'
+    // {guess} variable from input
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
