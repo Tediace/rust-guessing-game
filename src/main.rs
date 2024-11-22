19817e630ffa5cd33edfeb7bfc1da5ac3206966f
tediace <acetedi7@gmail.com>
2024-11-22 17:02:31 +0700

    Comparing the Guess to the Secret Number

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 use std::io;
 // rand defines that random number generator
 use rand::Rng;
+// another enum and has the variants Less, Greater, and Equal.
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -28,5 +30,24 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    // created new variable named guess
+    // rust allow us to shadow the previous value of guess with a new one
+    // shadowing lets us reuse the guess variable name rather than forcing us to create tow unique var
+    // the trim method on a string instance will eliminate any whitespace at the beginning and end
+    // if input 5 and presses enter, guess looks like this: 5\n
+    // the \n represents "new line"
+    // trim method eliminates \n or \r\n resulting in just 5
+    // SUMMARY parse method on string converts a string to another type
+    // the colon (:) after guess tells rust we'll annotate the variable type
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+    // match expression to decide what to do next based on which variant of Ordering
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!"),
+    }
 }
