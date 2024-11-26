6de61b714380a87be70b8043e8a1d633f7d368c5
tediace <acetedi7@gmail.com>
2024-11-26 10:01:49 +0700

    Allowing multiple guesses with looping

--- src/main.rs
+++ src/main.rs
@@ -14,40 +14,43 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..200);
 
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    // loop keyword created an infinite loop
+    loop {
+        println!("Please input your guess.");
 
-    // let for declaration variable
-    // mut for mutable variable
-    // guess is named variable
-    // string::new() created string blank
-    // Conclusion crate variable to store the user input
-    let mut guess = String::new( );
+        // let for declaration variable
+        // mut for mutable variable
+        // guess is named variable
+        // string::new() created string blank
+        // Conclusion crate variable to store the user input
+        let mut guess = String::new();
 
-    // io::stdin() get input from standard input (keyboard)
-    // .read_line(&mut guess) read input until press enter
-    // .expect if error throw message 'Failed to read line'
-    // {guess} variable from input
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // io::stdin() get input from standard input (keyboard)
+        // .read_line(&mut guess) read input until press enter
+        // .expect if error throw message 'Failed to read line'
+        // {guess} variable from input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // created new variable named guess
-    // rust allow us to shadow the previous value of guess with a new one
-    // shadowing lets us reuse the guess variable name rather than forcing us to create tow unique var
-    // the trim method on a string instance will eliminate any whitespace at the beginning and end
-    // if input 5 and presses enter, guess looks like this: 5\n
-    // the \n represents "new line"
-    // trim method eliminates \n or \r\n resulting in just 5
-    // SUMMARY parse method on string converts a string to another type
-    // the colon (:) after guess tells rust we'll annotate the variable type
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // created new variable named guess
+        // rust allow us to shadow the previous value of guess with a new one
+        // shadowing lets us reuse the guess variable name rather than forcing us to create two unique var
+        // the trim method on a string instance will eliminate any whitespace at the beginning and end
+        // if input 5 and presses enter, guess looks like this: 5\n
+        // the \n represents "new line"
+        // trim method eliminates \n or \r\n resulting in just 5
+        // SUMMARY parse method on string converts a string to another type
+        // the colon (:) after guess tells rust we'll annotate the variable type
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
-    // match expression to decide what to do next based on which variant of Ordering
+        println!("You guessed: {guess}");
+        // match expression to decide what to do next based on which variant of Ordering
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => println!("You win!"),
+            Ordering::Greater => println!("Too big!"),
+        }
     }
 }
